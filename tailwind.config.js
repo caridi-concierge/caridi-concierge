70861b7f27eddd4141229c0e6f756dbc8f2334bf
Joseph Deferio <22796581+jdeferio@users.noreply.github.com>
2025-09-15 18:07:09 -0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        fraunces: ["var(--font-fraunces)", "serif"],
+        satoshi: ["var(--font-satoshi)", "sans-serif"],
+      },
+    },
+  },
+}
