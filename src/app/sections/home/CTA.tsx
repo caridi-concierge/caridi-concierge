0c4004ab1e49867aae1c846a9f1409772b4685bb
Joseph Deferio <22796581+jdeferio@users.noreply.github.com>
2025-09-17 18:57:42 -0400

    add CTA to homepage

--- src/app/sections/home/CTA.tsx
+++ src/app/sections/home/CTA.tsx
@@ -4,14 +4,14 @@ import Button from "@/components/SecondaryButton";
 
 export default function CTA() {
   return (
-    <section className="bg-outer-space py-20">
-      <div className="container mx-auto text-center space-y-6">
-        <h2 className="font-fraunces text-3xl md:text-4xl font-bold text-alabaster">
+    <section className="bg-merino py-20">
+      <div className="container mx-auto bg-outer-space rounded-2xl px-8 py-16 text-center space-y-6">
+        <h2 className="font-fraunces text-7xl md:text-7xl text-alabaster">
           Modern beauty, made effortless.
         </h2>
-        <p className="text-alabaster/90 max-w-xl mx-auto">
-          Personalized Botox and filler treatments that restore balance, volume,
-          and confidence.
+        <p className="text-alabaster/90 text-lg max-w-xl mx-auto">
+          Personalized Botox and filler treatments that restore balance,
+          volume, and confidence.
         </p>
         <div className="pt-6">
           <Button
