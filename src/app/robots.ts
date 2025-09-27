426c647a3ae11e08d7f25ce6594b969e6ae105b9
Joseph Deferio <22796581+jdeferio@users.noreply.github.com>
2025-09-27 13:02:37 -0400

    add robots.txt and llms.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {userAgent: "*", allow: "/"},
+      {userAgent: "MJ12bot", disallow: "/"},
+      {userAgent: "DotBot", disallow: "/"},
+      {userAgent: "Baiduspider", disallow: "/"},
+      {userAgent: "Sogou", disallow: "/"},
+      {userAgent: "MegaIndex", disallow: "/"},
+      {userAgent: "BLEXBot", disallow: "/"},
+      {userAgent: "UptimeRobot", disallow: "/"},
+      {userAgent: "ScreamingFrog", disallow: "/"},
+      {userAgent: "HTTrack", disallow: "/"},
+      {userAgent: "SiteSucker", disallow: "/"},
+    ],
+    sitemap: "https://www.caridiconcierge.com/sitemap.xml",
+  };
+}
