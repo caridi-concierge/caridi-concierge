d3d8ea91975f9fe5f14a69503b39971bf45aa425
Joseph Deferio <22796581+jdeferio@users.noreply.github.com>
2025-10-30 21:11:33 -0400

    replace gbp link

--- src/lib/constants/company.ts
+++ src/lib/constants/company.ts
@@ -24,7 +24,7 @@ export const COMPANY = {
       linkedin: "https://www.linkedin.com/company/caridi-concierge/",
       tiktok: "https://tiktok.com/@caridiconcierge",
       yelp: "https://www.yelp.com/biz/caridi-concierge-brooklyn",
-      google: "https://maps.app.goo.gl/GdqFsMAwS6Q3ZWH88",
+      google: "https://www.google.com/maps/place/Caridi+Concierge/@40.716628,-73.9448356,11z/data=!3m1!4b1!4m6!3m5!1s0x252431c58addd917:0x5a531a6154fdd61d!8m2!3d40.716628!4d-73.9448356!16s%2Fg%2F11ybxlt6xz!5m2!1e1!1e4?entry=ttu&g_ep=EgoyMDI1MTAyOC4wIKXMDSoASAFQAw%3D%3D",
       pinterest: "https://www.pinterest.com/caridiconcierge/"
     },
   };
